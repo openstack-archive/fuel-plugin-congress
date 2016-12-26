8fd355d45a71b8f8727436db247264e2e1a30d27
Fedor Zhadaev <fzhadaev@mirantis.com>
2016-12-26 17:52:13 +0400

    Add DB-connection to the congress.pp manifest

--- deployment_scripts/puppet/manifests/congress.pp
+++ deployment_scripts/puppet/manifests/congress.pp
@@ -12,6 +12,25 @@ $rabbit_hosts       = split(hiera('amqp_hosts',''), ',')
 $rabbit_password    = $rabbit_hash['password']
 $rabbit_userid      = $rabbit_hash['user']
 
+$database_vip = hiera('database_vip', undef)
+$db_type      = 'mysql'
+$db_host      = pick($congress_hash['db_host'], $database_vip)
+$db_user      = pick($congress_hash['username'], 'congress')
+$db_password  = $congress_hash['db_password']
+$db_name      = pick($congress_hash['db_name'], 'congress')
+
+$db_connection = os_database_connection({
+  'dialect'  => $db_type,
+  'host'     => $db_host,
+  'database' => $db_name,
+  'username' => $db_user,
+  'password' => $db_password,
+  'charset'  => 'utf8'
+})
+
+class {'congress::db':
+  connection          => $db_connection
+}
 
 class { 'congress':
   rabbit_hosts        => $rabbit_hosts,
